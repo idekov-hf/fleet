82441e34dd7c2f00bd997936c9d696d8a74b9ecd
ivdekov <ivdekov@gmail.com>
2018-01-06 19:27:20 -0600

    Import firebase initialization in App component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import RootNavigator from './src/components/RootNavigator'
+import './src/utils/authentication/firebase-config.js'
 
 export default class App extends Component {
   render() {

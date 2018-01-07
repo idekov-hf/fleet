e9954176aba1e5fc2131a82d9db1215d26b4bab4
ivdekov <ivdekov@gmail.com>
2018-01-06 19:05:06 -0600

    Add Facebook signup method

--- src/utils/authentication/signup.js
+++ src/utils/authentication/signup.js
@@ -1,6 +1,24 @@
 import * as firebase from 'firebase'
+import { Facebook } from 'expo'
 import './firebase-config'
+import keys from './api-keys'
 
 export function emailSignUp(email, password) {
   return firebase.auth().createUserWithEmailAndPassword(email, password)
+}
+
+export async function facebookSignUp() {
+  const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+    keys.facebook,
+    {
+      permissions: ['public_profile']
+    }
+  )
+  if (type === 'success') {
+    // Get the user's name using Facebook's Graph API
+    const response = await fetch(
+      `https://graph.facebook.com/me?access_token=${token}`
+    )
+    return (await response.json()).name
+  }
 }
